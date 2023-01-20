f57ec9f427fd6f2603e6a9048a70b5b74e3b18cc
musstaffali <mamustafa@gtcc.edu>
2023-01-20 13:33:33 -0500

    bare bones of Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import { ThemeContext } from '../GlobalComponents/ThemeProvider';
-import { BiSun, BiMoon, BiCart} from 'react-icons/bi';
-import { VscAccount } from 'react-icons/vsc';
+import {BiCart} from 'react-icons/bi';
 import { Link } from "@reach/router";
 import { useCart } from "react-use-cart";
 
@@ -36,15 +35,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Link to="sign-in" className={`nav-link ${darkMode? 'text-dark-primary' : 'text-light-primary'}`}>
-                Sign in
-              </Link>
-              <Nav.Link 
-                className={darkMode? 'text-dark-primary': 'text-light-primary'}
-                onClick={()=>setDarkMode(!darkMode)}
-              >
-                {darkMode? <BiSun size="1.7rem" />: <BiMoon size="1.7rem" />}
-              </Nav.Link>
+
               <Link
                 to="/cart"
                 className={`${darkMode? 'text-dark-primary': 'text-light-primary'} d-flex align-items-center`}
@@ -53,10 +44,7 @@ const Header = () => {
                 {!isEmpty && <span style={{ position: 'relative', left: '-21px', top: '-18px'}}>{totalItems}</span>}
                 <span style={{ marginLeft: !isEmpty ? '-13px': 0}}>&nbsp;Cart</span>
               </Link>
-              <Link to="my-account" className={`nav-link ${darkMode? 'text-dark-primary': 'text-light-primary'}`}>
-                  <VscAccount size="1.8rem"/>
-                  &nbsp;My Account
-              </Link>
+
             </Nav>
           </Navbar.Collapse>
         </Container>
